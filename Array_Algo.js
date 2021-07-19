316664ac95bb1c58c1bae7dd6f5627ee9c8722a9
jcambray10 <84952483+jcambray10@users.noreply.github.com>
2021-07-18 21:19:48 -0500

    second commit

--- Array_Algo.js
+++ Array_Algo.js
@@ -1,7 +1,8 @@
-let arr = [6,3,5,1,2,4]
-for(let i=1; i< arr.length; i++){
-    sum+=i
-    console.log("Num:", i, "Sum:", sum );
+testArr = [6,3,5,1,2,4]
+var sum=0;
+for(let i=0; i<testArr.length; i++){
+    sum+=testArr[i];
+    console.log("Num:", testArr[i], "Sum:", sum );
 }
 
 // Expected Outcome
@@ -11,3 +12,21 @@ for(let i=1; i< arr.length; i++){
 // Num 1, Sum 15
 // Num 2, Sum 17
 // Num 4, Sum 21
+
+// echo "# Arrays_Algo" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/jcambray10/Arrays_Algo.git
+// git push -u origin main
+
+testArr = [6,3,5,1,2,4]
+var sum=0;
+for(let i=0; i<testArr.length; i++){
+    testArr[i] = testArr[i] * i;
+}
+console.log(testArr);
+
+// Expected Outcome Console.log
+// [0,3,10,3,8,20]
