e4f12648a76dc6b27b78cb59128ce410d9d7896e
jcambray10 <84952483+jcambray10@users.noreply.github.com>
2021-07-18 21:06:23 -0500

    first commit

---
+++ Array_Algo.js
@@ -0,0 +1,13 @@
+let arr = [6,3,5,1,2,4]
+for(let i=1; i< arr.length; i++){
+    sum+=i
+    console.log("Num:", i, "Sum:", sum );
+}
+
+// Expected Outcome
+// Num 6, Sum 6
+// Num 3, Sum 9
+// Num 5, Sum 14
+// Num 1, Sum 15
+// Num 2, Sum 17
+// Num 4, Sum 21
